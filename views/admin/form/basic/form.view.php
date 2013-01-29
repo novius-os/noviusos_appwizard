95cc3b067f322b8186f61eeb1709eacd1579ec0a
Sébastien Drouyer <drouyer@novius.com>
2013-01-29 17:52:24 +0100

    updated appwizard css

--- views/admin/form/basic/form.view.php
+++ views/admin/form/basic/form.view.php
@@ -1,3 +1,25 @@
+<?php
+$i18n = array(
+    'Step {{num}}. ({{modelName}}) Fields layout' => __('Step {{num}}. ({{modelName}}) Fields layout'),
+    'Step {{num}}. ({{modelName}}) Fields' => __('Step {{num}}. ({{modelName}}) Fields'),
+    'Step {{num}}. Fields layout' => __('Step {{num}}. Fields layout'),
+    'Step {{num}}. Fields' => __('Step {{num}}. Fields'),
+    'Step {{num}}. Compile' => __('Step {{num}}. Compile'),
+);
+
+$templates = array(
+    'model' => render('noviusos_appwizard::admin/form/basic/model', array('config' => $config), false),
+    'field' => render('noviusos_appwizard::admin/form/basic/field', array('config' => $config), false),
+    'categories' => render('noviusos_appwizard::admin/form/basic/categories', array('config' => $config), false),
+    'category' => render('noviusos_appwizard::admin/form/basic/category', array('config' => $config), false),
+    'fields' => render('noviusos_appwizard::admin/form/basic/fields', array('config' => $config), false),
+);
+
+$options = array(
+    'i18n' => $i18n,
+    'templates' => $templates
+);
+?>
 <h1 class="appwizard">
     <?= __('‘Build your app’ wizard') ?>
 </h1>
@@ -32,102 +54,13 @@
         </div>
     </div>
 </form>
-
-
-<style type="text/css">
-    .models_list .field_item {
-        border: 1px solid grey;
-        padding: 10px;
-        margin-bottom: 15px;
-    }
-
-    .crud_options, .column_options {
-        margin-left: 20px;
-    }
-
-    .crud_options.inactive, .crud_other_options.inactive, .is_title_area.inactive {
-        display: none;
-    }
-
-    .installation_successful {
-        margin: 20px;
-        margin-top: 35px;
-        padding: 10px;
-        display: none;
-    }
-
-    .installation_successful.done {
-        display: block;
-    }
-
-    .installation_successful h2 {
-        font-size: 20px;
-    }
-
-    .installation_successful .sql {
-        margin-top: 5px;
-    }
-
-    #general_application_settings hr {
-        margin-top: 20px;
-        margin-bottom: 20px;
-    }
-
-    h1.appwizard {
-        margin-top: 6px;
-        margin-left: 4%;
-        font-size: 18px;
-    }
-
-    .appwizard label {
-        display: block;
-    }
-
-    .appwizard label.inline, .appwizard label.tooltip {
-        display: inline;
-    }
-
-    .field_item > .labelled_input > label {
-        margin-top: 10px;
-        margin-bottom: 5px;
-    }
-
-    .field_item .visible_where {
-        margin-top: 15px;
-    }
-</style>
-
-<?php
-    $i18n = array(
-        'Step {{num}}. ({{modelName}}) Fields layout' => __('Step {{num}}. ({{modelName}}) Fields layout'),
-        'Step {{num}}. ({{modelName}}) Fields' => __('Step {{num}}. ({{modelName}}) Fields'),
-        'Step {{num}}. Fields layout' => __('Step {{num}}. Fields layout'),
-        'Step {{num}}. Fields' => __('Step {{num}}. Fields'),
-        'Step {{num}}. Compile' => __('Step {{num}}. Compile'),
-    );
-
-    $templates = array(
-        'model' => render('noviusos_appwizard::admin/form/basic/model', array('config' => $config), false),
-        'field' => render('noviusos_appwizard::admin/form/basic/field', array('config' => $config), false),
-        'categories' => render('noviusos_appwizard::admin/form/basic/categories', array('config' => $config), false),
-        'category' => render('noviusos_appwizard::admin/form/basic/category', array('config' => $config), false),
-        'fields' => render('noviusos_appwizard::admin/form/basic/fields', array('config' => $config), false),
-    );
-
-    $options = array(
-        'i18n' => $i18n,
-        'templates' => $templates
-    );
-?>
-
 <script type="text/javascript">
-
-
     require(
             [
                 'jquery-nos',
                 'wijmo.wijtabs',
-                'static/apps/noviusos_appwizard/js/jquery.appwizard.js'
+                'static/apps/noviusos_appwizard/js/jquery.appwizard.js',
+                'link!static/apps/noviusos_appwizard/css/appwizard.css'
             ],
             function($) {
                 var $form = $('#<?= $form_id ?>');
