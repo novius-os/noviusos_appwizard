09c427627eb11ca608a6f6e6eb88e4b9f4778282
Julian Esperat <esperat@novius.fr>
2013-01-30 15:49:00 +0100

    "Updated translations"

---
+++ lang/fr/common.lang.php
@@ -0,0 +1,146 @@
+<?php
+
+// Generated on 29/01/2013 19:08:29
+
+// 0 out of 44 messages are translated (0%).
+// 0 out of 159 words are translated (0%).
+
+return array(
+    #: classes/controller/admin/application.ctrl.php:23
+    'Bravo! Your application has been created. We hope you’ll like it.' => '',
+
+    #: views/admin/form/basic/model_inside.view.php:5
+    'Name (e.g. Monkey):' => '',
+
+    #: views/admin/form/basic/model_inside.view.php:11
+    'Table name (e.g. monkeys):' => '',
+
+    #: views/admin/form/basic/model_inside.view.php:17
+    'Column prefix (e.g. monk_):' => '',
+
+    #: views/admin/form/basic/models.view.php:4
+    #: views/admin/form/basic/fields.view.php:4
+    #: views/admin/form/basic/categories.view.php:4
+    'Next step' => '',
+
+    #: views/admin/form/basic/models.view.php:4
+    #: views/admin/form/basic/fields.view.php:4
+    #: views/admin/form/basic/categories.view.php:4
+    'or' => '',
+
+    #: views/admin/form/basic/models.view.php:4
+    'Add another model' => '',
+
+    #: views/admin/form/basic/form.view.php:2
+    '‘Build your app’ wizard' => '',
+
+    #: views/admin/form/basic/form.view.php:7
+    'Step 1. Main properties' => '',
+
+    #: views/admin/form/basic/form.view.php:8
+    'Step 2. Create' => '',
+
+    #: views/admin/form/basic/form.view.php:12
+    'About the application' => '',
+
+    #: views/admin/form/basic/form.view.php:20
+    'Options' => '',
+
+    #: views/admin/form/basic/form.view.php:23
+    'Generate' => '',
+
+    #: views/admin/form/basic/form.view.php:26
+    'Now that you have a brand new application' => '',
+
+    #: views/admin/form/basic/form.view.php:29
+    'What is next? Sql installation file is located at install.sql.' => '',
+
+    #: views/admin/form/basic/form.view.php:333
+    'Step {{num}}. ({{modelName}}) Fields layout' => '',
+
+    #: views/admin/form/basic/form.view.php:334
+    'Step {{num}}. ({{modelName}}) Fields' => '',
+
+    #: views/admin/form/basic/form.view.php:336
+    'Step {{num}}. Fields layout' => '',
+
+    #: views/admin/form/basic/form.view.php:337
+    'Step {{num}}. Fields' => '',
+
+    #: views/admin/form/basic/form.view.php:341
+    'Step {{num}}. Compile' => '',
+
+    #: views/admin/form/basic/category_inside.view.php:5
+    'Title (e.g. Properties):' => '',
+
+    #: views/admin/form/basic/category_inside.view.php:11
+    'Type:' => '',
+
+    #: views/admin/form/basic/field.view.php:3
+    'Field' => '',
+
+    #: views/admin/form/basic/fields.view.php:4
+    'Add another field' => '',
+
+    #: views/admin/form/basic/field_inside.php:4
+    'Label (e.g. Title):' => '',
+
+    #: views/admin/form/basic/field_inside.php:10
+    'Column name (without prefix; e.g. title):' => '',
+
+    #: views/admin/form/basic/field_inside.php:15
+    'Use label' => '',
+
+    #: views/admin/form/basic/field_inside.php:21
+    'Type' => '',
+
+    #: views/admin/form/basic/field_inside.php:35
+    'Is the form title' => '',
+
+    #: views/admin/form/basic/field_inside.php:41
+    'Shows in the main grid of the App Desk' => '',
+
+    #: views/admin/form/basic/field_inside.php:47
+    'Shows in the add/update forms' => '',
+
+    #: views/admin/form/basic/field_inside.php:54
+    #: views/admin/form/basic/category.view.php:3
+    'Field group' => '',
+
+    #: views/admin/form/basic/generate_options.view.php:3
+    'Install the application on this Novius OS?' => '',
+
+    #: views/admin/form/basic/categories.view.php:4
+    'Add another field group' => '',
+
+    #: views/admin/form/basic/application_settings.view.php:3
+    'Application name (e.g. My Application):' => '',
+
+    #: views/admin/form/basic/application_settings.view.php:9
+    'Application folder (e.g. my_application):' => '',
+
+    #: views/admin/form/basic/application_settings.view.php:15
+    'Application namespace (e.g. MyApplication):' => '',
+
+    #: views/admin/form/basic/model.view.php:3
+    'Model' => '',
+
+    #: config/controller/admin/application.config.php:98
+    'Main column' => '',
+
+    #: config/controller/admin/application.config.php:101
+    'Side column' => '',
+
+    #: config/controller/admin/application.config.php:106
+    'Single line text' => '',
+
+    #: config/controller/admin/application.config.php:109
+    'WYSIWYG text editor' => '',
+
+    #: config/controller/admin/application.config.php:112
+    'Paragraph text' => '',
+
+    #: config/controller/admin/application.config.php:115
+    'Checkbox' => '',
+
+);
