1703e445174203c251cd5f95d1ce2a652edca784
Julian Esperat <esperat@novius.fr>
2013-04-09 16:37:43 +0200

    "Adding Japanese\!"

---
+++ lang/ja/common.lang.php
@@ -0,0 +1,185 @@
+<?php
+
+// Generated on 09/04/2013 16:13:10
+
+// 56 out of 56 messages are translated (100%).
+// 225 out of 225 words are translated (100%).
+
+return array(
+    #: classes/controller/admin/application.ctrl.php:30
+    'Bravo! Your application has been created. If you’ve chosen to install it on this Novius OS, go to the home tab to try it out.' => 'おめでとうございます。アプリケーションが作成されました。このアプリケーションを Novius OS にインストールするには、ホームタブに移動して実行してください。',
+
+    #: config/controller/admin/application.config.php:152
+    'Main column' => 'メインカラム',
+
+    #: config/controller/admin/application.config.php:155
+    'Side column' => 'サイドカラム',
+
+    #: config/controller/admin/application.config.php:160
+    'Single line text' => 'テキスト(一行)',
+
+    #: config/controller/admin/application.config.php:163
+    'WYSIWYG text editor' => 'ビジュアルエディタ',
+
+    #: config/controller/admin/application.config.php:166
+    'Paragraph text' => 'テキスト(複数行)',
+
+    #: config/controller/admin/application.config.php:169
+    'Checkbox' => 'チェックボックス',
+
+    #: config/controller/admin/application.config.php:172
+    'Image from Media Centre' => 'メディアセンターの画像',
+
+    #: views/admin/generation/basic/views/front/model_item.view.php:39
+    'Back' => '戻る',
+
+    #: views/admin/form/basic/field.view.php:5
+    'Field' => 'フィールド',
+
+    #: views/admin/form/basic/category_inside.view.php:8
+    'Title (e.g. Properties):' => 'タイトル(例: プロパティ) :',
+
+    #: views/admin/form/basic/category_inside.view.php:14
+    #: views/admin/form/basic/field_inside.php:25
+    'Type:' => 'タイプ :',
+
+    #: views/admin/form/basic/model_inside.view.php:8
+    'Name (e.g. Monkey):' => '名前(例: モンキー) :',
+
+    #: views/admin/form/basic/model_inside.view.php:14
+    'Table name (e.g. monkeys):' => 'テーブル名(例: monkeys) :',
+
+    #: views/admin/form/basic/model_inside.view.php:20
+    'Column prefix (e.g. monk_):' => 'カラムプレフィックス(例: monk_) :',
+
+    #: views/admin/form/basic/model_inside.view.php:26
+    'Add an URL enhancer?' => 'URL エンハンサを追加しますか？',
+
+    #: views/admin/form/basic/generate_options.view.php:6
+    'Install the application on this Novius OS?' => 'この Novius OS にアプリケーションをインストールしますか？',
+
+    #: views/admin/form/basic/model.view.php:5
+    'Model' => 'モデル',
+
+    #: views/admin/form/basic/field_inside.php:7
+    'Label (e.g. Title):' => 'ラベル(例: タイトル) :',
+
+    #: views/admin/form/basic/field_inside.php:13
+    'Column name (without prefix; e.g. title):' => 'カラム名(プレフィックスは付けない、例: title) :',
+
+    #: views/admin/form/basic/field_inside.php:19
+    'Use label' => 'ラベルを使用する',
+
+    #: views/admin/form/basic/field_inside.php:39
+    'Is the form title' => 'フォームのタイトル',
+
+    #: views/admin/form/basic/field_inside.php:45
+    'Shows in the main grid of the App Desk' => 'App Deskのメイングリッドに表示する',
+
+    #: views/admin/form/basic/field_inside.php:51
+    'Shows in the add/update forms' => '追加／更新フォームに表示する',
+
+    #: views/admin/form/basic/field_inside.php:58
+    #: views/admin/form/basic/category.view.php:5
+    'Fields group' => 'フィールドグループ',
+
+    #: views/admin/form/basic/fields.view.php:7
+    #: views/admin/form/basic/categories.view.php:7
+    #: views/admin/form/basic/models.view.php:7
+    'Next step' => '次のステップ',
+
+    #: views/admin/form/basic/fields.view.php:7
+    #: views/admin/form/basic/categories.view.php:7
+    #: views/admin/form/basic/models.view.php:7
+    'or' => 'または',
+
+    #: views/admin/form/basic/fields.view.php:7
+    'Add another field' => 'フィールドを追加する',
+
+    #: views/admin/form/basic/form.view.php:7
+    'Step {{num}}. ({{modelName}}) Fields layout' => 'ステップ {{num}}。({{modelName}}) フィールドレイアウト',
+
+    #: views/admin/form/basic/form.view.php:8
+    'Step {{num}}. ({{modelName}}) Fields' => 'ステップ {{num}}。 ({{modelName}}) フィールド',
+
+    #: views/admin/form/basic/form.view.php:9
+    'Step {{num}}. Fields layout' => 'ステップ {{num}}。 フィールドレイアウト',
+
+    #: views/admin/form/basic/form.view.php:10
+    'Step {{num}}. Fields' => 'ステップ {{num}}。 フィールド',
+
+    #: views/admin/form/basic/form.view.php:11
+    'Step {{num}}. Compile' => 'ステップ {{num}}。 コンパイル',
+
+    #: views/admin/form/basic/form.view.php:29
+    #: views/admin/form/basic/form.view.php:111
+    '‘Build your app’ wizard' => '「アプリケーション作成」ウィザード',
+
+    #: views/admin/form/basic/form.view.php:34
+    'Step 1. Main properties' => 'ステップ 1。メインプロパティ',
+
+    #: views/admin/form/basic/form.view.php:39
+    'About the application' => 'このアプリケーションについて',
+
+    #: views/admin/form/basic/form.view.php:47
+    'Options' => 'オプション',
+
+    #: views/admin/form/basic/form.view.php:50
+    'Generate' => '生成する',
+
+    #: views/admin/form/basic/form.view.php:53
+    'You now have a brand new application. What’s next?' => 'アプリケーションが新規作成されました。さあ、次は何？',
+
+    #: views/admin/form/basic/form.view.php:57
+    '(FYI: the SQL installation file is to be found at the root of the application.)' => '(参考: SQL インストールファイルは、アプリケーションのルートにあります。)',
+
+    #: views/admin/form/basic/form.view.php:60
+    'You may now:' => 'お好みでどうぞ。',
+
+    #. Note to translator: Link to Novius OS documentation. Only the subdomain has to be changed, e.g. docs-fr.novius-os.org.
+    #: views/admin/form/basic/form.view.php:67
+    'http://docs.novius-os.org/en/0.2/app_extend/add_field.html' => 'http://docs.novius-os.org/en/0.2/app_extend/add_field.html',
+
+    #: views/admin/form/basic/form.view.php:68
+    'Add a new field to your application.' => 'アプリケーションに、新規フィールドを追加する',
+
+    #: views/admin/form/basic/form.view.php:72
+    'http://docs.novius-os.org/en/0.2/app_extend/change_appearance.html' => 'http://docs.novius-os.org/en/0.2/app_extend/change_appearance.html',
+
+    #: views/admin/form/basic/form.view.php:73
+    'Change the default display in the front-office.' => 'フロントオフィスのデフォルト表示を変更します。',
+
+    #: views/admin/form/basic/form.view.php:77
+    'http://docs.novius-os.org/en/0.2/app_extend/add_action.html' => 'http://docs.novius-os.org/en/0.2/app_extend/add_action.html',
+
+    #: views/admin/form/basic/form.view.php:78
+    'Add a new action in the back-office.' => 'バックオフィスに新しいアクションを追加します。',
+
+    #: views/admin/form/basic/form.view.php:82
+    'http://docs.novius-os.org/en/0.2/app_extend/front_event.html' => 'http://docs.novius-os.org/en/0.2/app_extend/front_event.html',
+
+    #: views/admin/form/basic/form.view.php:83
+    'Change a behaviour in the front-office.' => 'フロントオフィスの動作を変更します。',
+
+    #: views/admin/form/basic/form.view.php:87
+    'http://docs.novius-os.org/en/0.2/app_extend/extending.html' => 'http://docs.novius-os.org/en/0.2/app_extend/extending.html',
+
+    #: views/admin/form/basic/form.view.php:88
+    'Learn how to extend your application.' => 'アプリケーションを拡張する方法を学ぶ',
+
+    #: views/admin/form/basic/categories.view.php:7
+    'Add another fields group' => 'フィールドグループを追加する',
+
+    #: views/admin/form/basic/models.view.php:7
+    'Add another model' => 'モデルを追加する',
+
+    #: views/admin/form/basic/application_settings.view.php:6
+    'Application name (e.g. My Application):' => 'アプリケーション名(例: マイアプリケーション) :',
+
+    #: views/admin/form/basic/application_settings.view.php:12
+    'Application folder (e.g. my_application):' => 'アプリケーションフォルダ(例: my_application) :',
+
+    #: views/admin/form/basic/application_settings.view.php:18
+    'Application namespace (e.g. MyApplication):' => 'アプリケーションの名前空間(例: MyApplication) :',
+
+);
