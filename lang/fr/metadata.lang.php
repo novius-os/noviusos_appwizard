6c3e7f103933f5922818e7543ac9140a6b89f71d
Julian Esperat <esperat@novius.fr>
2013-04-17 14:54:14 +0200

    "Updating French translations"

--- lang/fr/metadata.lang.php
+++ lang/fr/metadata.lang.php
@@ -1,12 +1,11 @@
 <?php
 
-// Generated on 20/02/2013 17:43:50
+// Generated on 17/04/2013 14:49:11
 
 // 1 out of 1 messages are translated (100%).
 // 4 out of 4 words are translated (100%).
 
 return array(
-    #. Overwritten
     '‘Build your app’ wizard' => 'Assistant ‘Créer mon appli’',
 
 );
