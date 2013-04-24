633569bf32ee5ff158ebbd07bf8824d5d219eb93
Julian Esperat <esperat@novius.fr>
2013-04-24 18:02:00 +0200

    "Updating Japanase translations"

---
+++ lang/ja/metadata.lang.php
@@ -0,0 +1,11 @@
+<?php
+
+// Generated on 24/04/2013 17:47:52
+
+// 1 out of 1 messages are translated (100%).
+// 4 out of 4 words are translated (100%).
+
+return array(
+    '‘Build your app’ wizard' => '「アプリケーション作成」ウィザード',
+
+);
