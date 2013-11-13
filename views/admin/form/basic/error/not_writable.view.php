78d1b06052c3960b386e7cc8ca6b647a728ea204
Sébastien Drouyer <drouyer@novius.com>
2013-11-13 17:29:40 +0100

    added comments and scrutinizer stuff

--- views/admin/form/basic/error/not_writable.view.php
+++ views/admin/form/basic/error/not_writable.view.php
@@ -1,3 +1,13 @@
+<?php
+/**
+ * NOVIUS OS - Web OS for digital communication
+ *
+ * @copyright  2011 Novius
+ * @license    GNU Affero General Public License v3 or (at your option) any later version
+ *             http://www.gnu.org/licenses/agpl-3.0.html
+ * @link http://www.novius-os.org
+ */
+?>
 <h1 class="appwizard">
     <?= __('‘Build your app’ wizard') ?>
 </h1>
