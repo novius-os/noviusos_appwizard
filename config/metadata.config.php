9349b2f60a9cf6f51df0e8b5e7cef45ee32b9f65
Gilles FELIX <felix@novius.com>
2014-03-06 10:58:28 +0100

    "Version 4.1 (Dubrovka)"

--- config/metadata.config.php
+++ config/metadata.config.php
@@ -10,7 +10,7 @@
 
 return array(
     'name'    => '‘Build your app’ wizard',
-    'version' => 'dubrovka',
+    'version' => '4.1 (Dubrovka)',
     'provider' => array(
         'name' => 'Novius OS',
     ),
